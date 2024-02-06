76ba3b7171a045ac47093f50a9c875be0ed1693e
Lofton <lofton@tabl.social>
2024-02-05 20:16:21 -0500

    more values are now boolean based

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,110 +3,122 @@ import { Box, CircularProgress, TextField, Tooltip } from '@mui/material'
 import SearchBarFilter from './Components/SearchBarFilter'
 import SearchBarIcon from './Components/SearchBarIcon'
 
-const searchBarTip = 'tip'
-const searchIconTip = 'tip'
-const searchIconColor = 'blue'
-const filters = ['test']
-const defaultFilter = 'test'
-const loading = false
-
 const SearchBar = forwardRef((props, ref) => {
-  const {
-    data,
-    onSearch,
-    //searchBarTip,
-    //searchIconTip,
-    //searchIconColor,
-    //filters,
-    //defaultFilter,
-    //loading
-  } = props
-  const [searchTerm, setSearchTerm] = useState('')
-  const [focused, isFocused] = useState(false)
-  const [filter, setFilter] = useState(defaultFilter)
+  try {
+    const {
+      data,
+      onSearch,
+      filtering = true,
+      searchBarIcon = true,
+      searchBarTip = 'tip',
+      searchIconTip = 'tip',
+      searchIconColor = 'blue',
+      filters = ['test'],
+      defaultFilter = 'test',
+      loading = false
+    } = props
 
-  //Boolean to determine if exact match
-  const isExactMatch = (value, exactMatch) => value && value.toString().toLowerCase() === exactMatch
-
-  //Boolen to determine if term is inside the array of objects
-  const containSearchTerm = (value, term) => value && value.toString().toLowerCase().includes(term)
-
-  const handleSearch = () => {
-    const term = searchTerm.trim().toLowerCase()
-    const searchData = data
-
-    if (term.startsWith('"') && term.endsWith('"')) {
-      const exactMatch = term.slice(1, -1)
-
-      const results = searchData.filter(row =>
-        Object.entries(row).some(([key, value]) => (
-          key === filter && isExactMatch(value, exactMatch))
-        )
-      )
-
-      onSearch(results)
-    } else {
-      const results = searchData.filter((row) => {
-        Object.entries(row).some(([key, value]) =>
-          key === filter && containSearchTerm(value, term)
-        )
-      })
-
-      onSearch(results)
+    //Error thrown to ensure everything is properly set when filtering
+    if (filtering && (!defaultFilter || !Array.isArray(filters))) {
+      throw new Error('Invalid Configuration: When filtering is set to true, a default filter value must be set, and filters must be an array')
     }
-  }
 
-  const handleFilterChange = (newFilter) => {
-    setFilter(newFilter)
-  }
+    //Warning thrown to ensure that the filters array includes the default filter value the user set
+    if (filtering && defaultFilter && !filters?.includes(defaultFilter)) {
+      console.warn(`'${defaultFilter}' is not a valid value in the 'filters' array`)
+      console.warn(`Stack Trace: ${new Error().stack}`)
+    }
 
-  return (
-    <Tooltip title={focused ? searchBarTip : ''}>
-      <TextField
-        type='search'
-        ref={ref}
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        onFocus={() => isFocused(true)}
-        onBlur={() => isFocused(false)}
-        onKeyPress={(e) => {
-          if (event.key === 'Enter') {
-            handleSearch()
-          }
-        }}
-        sx={{
-          '.MuiOutlinedInput-input': {
-            marginRight: '12px'
-          }
-        }}
-        InputProps={{
-          endAdornment: (
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: '10px' }}>
-              <SearchBarFilter
-                filters={filters}
-                defaultFilter={defaultFilter}
-                handleFilterChange={handleFilterChange}
-              />
-              {loading ?
-                (
-                  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <CircularProgress sx={{ scale: '60%' }} />
-                  </Box>
-                ) : (
-                  <SearchBarIcon
-                    searchIconTip={searchIconTip}
-                    searchIconColor={searchIconColor}
-                    handleSearch={handleSearch}
+    const [searchTerm, setSearchTerm] = useState('')
+    const [focused, isFocused] = useState(false)
+    const [filter, setFilter] = useState(defaultFilter)
+
+    //Boolean to determine if exact match
+    const isExactMatch = (value, exactMatch) => value && value.toString().toLowerCase() === exactMatch
+
+    //Boolen to determine if term is inside the array of objects
+    const containSearchTerm = (value, term) => value && value.toString().toLowerCase().includes(term)
+
+    const handleSearch = () => {
+      const term = searchTerm.trim().toLowerCase()
+      const searchData = data
+
+      if (term.startsWith('"') && term.endsWith('"')) {
+        const exactMatch = term.slice(1, -1)
+
+        const results = searchData.filter((row) =>
+          Object.entries(row).some(([key, value]) => filtering ? (key === filter && isExactMatch(value, exactMatch)) : (isExactMatch(value, exactMatch)))
+        )
+
+        onSearch(results)
+      } else {
+        const results = searchData.filter((row) => {
+          Object.entries(row).some(([key, value]) => filtering ? (key === filter && containSearchTerm(value, term)) : (containSearchTerm(value, term)))
+        })
+
+        onSearch(results)
+      }
+    }
+
+    const handleFilterChange = (newFilter) => {
+      setFilter(newFilter)
+    }
+
+    return (
+      <Tooltip title={focused ? searchBarTip : ''}>
+        <TextField
+          type='search'
+          ref={ref}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onFocus={() => isFocused(true)}
+          onBlur={() => isFocused(false)}
+          onKeyPress={(e) => {
+            if (event.key === 'Enter') {
+              handleSearch()
+            }
+          }}
+          sx={{
+            '.MuiOutlinedInput-input': {
+              marginRight: '12px'
+            }
+          }}
+          InputProps={{
+            endAdornment: (
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: '10px' }}>
+                {filtering && (
+                  <SearchBarFilter
+                    filters={filters}
+                    defaultFilter={defaultFilter}
+                    handleFilterChange={handleFilterChange}
                   />
                 )}
-            </Box>
-          )
-        }}
-        disabled={loading}
-        size='small'
-      />
-    </Tooltip>
-  )
+                {loading ?
+                  (
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                      <CircularProgress sx={{ scale: '60%' }} />
+                    </Box>
+                  ) : (
+                    searchBarIcon && (
+                      <SearchBarIcon
+                        searchIconTip={searchIconTip}
+                        searchIconColor={searchIconColor}
+                        handleSearch={handleSearch}
+                      />
+                    )
+                  )}
+              </Box>
+            )
+          }}
+          disabled={loading}
+          size='small'
+        />
+      </Tooltip>
+    )
+  } catch (error) {
+    console.error(`Error: ${error}`)
+    console.error(`Stack Trace ${error.stack}`)
+  }
 })
 
 export default SearchBar
@@ -126,16 +138,29 @@ export default SearchBar
  *  6. Can change ONLY the color of the search icon when hovering over it
  *  7. loading boolean disables the TextField when the data is being searched through and shows a circular progress animation
  *  8. Can pass MUI TextField props ONLY to the SearchBar component
+ * 
+ *  Completed TODOS:
+ *  1.
+ *  2.
+ *  3. Providing filters and defaultFilterValue is boolean (but if providing filters, must be as an array, and defaultFilterValue must appear in the array and be set)
+ *  4. SearchBarIcon is boolean
+ *  5.
+ *  6.
+ *  7.
+ *  8.
+ *  9.
+ *  10.
  */
 
 /**
  * TODO:
  *  1. User can search through an array of arrays for specified searchTerm and return array of arrays (maybe also singular array)
  *  2. Change TextField with Autocomplete to show suggestions in the text box
- *  3. Providing filters and defaultFilterValue is boolean (but if providing filters, must be as an array, and defaultFilterValue must appear in the array and be set)
- *  4. SearchBarIcon is boolean
  *  5. Can adjust the Select in SearchBarFilter component (it's possible to pass MUI components to it)
  *  6. Can change/style the SearchIcon in SearchBarIcon (it's possible to pass MUI components to it)
  *  7. Multi-select filters (Autocomplete)
  *  8. Must specify if parsing through an array of arrays or array of Objects through a boolean (if array of arrays, can't use filters)
+ *  9. Error handling for each of the components
+ *  10. Improved warning messages
+ *  11. Make more values boolean, so not everything has to be specified
  */
